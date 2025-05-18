4db2c6e384791e39e0f4d3af59b04343901e7309
DhenSouza <denilson_contato@outlook.com>
2025-05-18 17:52:44 -0300

    update pom and feign use for requisition via-cep api

--- src/main/java/com/yourcompany/usermanagement/user_management_service/infrastructure/externalServices/viaCepApi/CepService.java
+++ src/main/java/com/yourcompany/usermanagement/user_management_service/infrastructure/externalServices/viaCepApi/CepService.java
@@ -3,30 +3,22 @@ package com.yourcompany.usermanagement.user_management_service.infrastructure.ex
 import com.yourcompany.usermanagement.user_management_service.application.web.errorHandler.ExternalServiceException;
 import com.yourcompany.usermanagement.user_management_service.infrastructure.externalServices.viaCepApi.dto.ViaCepResponse;
 import com.yourcompany.usermanagement.user_management_service.infrastructure.externalServices.viaCepApi.interfaces.ICepService;
-import com.yourcompany.usermanagement.user_management_service.application.web.address.dto.AddressCepResponse;
+import com.yourcompany.usermanagement.user_management_service.infrastructure.externalServices.viaCepApi.interfaces.ViaCepClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 @RequiredArgsConstructor
 public class CepService implements ICepService {
 
-    private final WebClient webClient = WebClient.builder()
-            .baseUrl("https://viacep.com.br/ws")
-            .build();
+    private final ViaCepClient viaCepClient;
 
+    @Override
     public ViaCepResponse getCodePostal(String cep) {
         try {
-            return webClient.get()
-                    .uri("/{cep}/json/", cep)
-                    .retrieve()
-                    .bodyToMono(ViaCepResponse.class)
-                    .block();
-        }catch (Exception e){
-            throw new ExternalServiceException("Error consult service ViaCep", e);
+            return viaCepClient.getAddressByCep(cep);
+        } catch (Exception e) {
+            throw new ExternalServiceException("Error consulting service ViaCep", e);
         }
     }
-
-
 }
